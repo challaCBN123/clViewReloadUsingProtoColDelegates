2067432cb99e16364ddcfd254a7703dad62fe359
challaCBN123 <challabhargava55@gmail.com>
2020-08-13 19:23:33 +0530

    Initial Commit

---
+++ clViewReloadUsingProtoColDelegates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  clViewReloadUsingProtoColDelegates
+//
+//  Created by Bhargava on 13/08/20.
+//  Copyright © 2020 Bhargava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
